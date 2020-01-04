2ba6fad01da3c6c52b1b881fb58ba6bb886a4433
Muhammed <muhammed10200802@outlook.com>
2020-01-03 18:38:32 -0800

    SAD

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -17,7 +17,6 @@ const Toolbar = props => (
                 <div className = 'options' id = 'start'><i class="fas fa-home"></i>Start</div>
                 <div className = 'options' id = 'apps'>
                     <i class="fas fa-bars"></i>Web apps<i id = 'down' class="fas fa-angle-down"></i>
-                
                 </div>
             </div>
             <div className = 'sadrzaj'>
