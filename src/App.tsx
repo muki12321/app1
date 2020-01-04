86b8e3dd69c81a6253abeb0c313d7d8fa163a193
Muhammed <muhammed10200802@outlook.com>
2020-01-03 21:27:23 -0800

    Sutra nastavljamo Ako Bog Da

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import Toolbar from './components/Toolbar/Toolbar';
 
-const App: React.FC = () => {
-  return (
+class App extends Component{
+  render() {
+    return (
     <div className="App">
       <Toolbar />
     </div>
-  );
+    )
+  }
+  
 }
 
 export default App;
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -16,7 +16,15 @@ const Toolbar = props => (
             <div className = 'menu'>
                 <div className = 'options' id = 'start'><i class="fas fa-home"></i>Start</div>
                 <div className = 'options' id = 'apps'>
-                    <i class="fas fa-bars"></i>Web apps<i id = 'down' class="fas fa-angle-down"></i>
+                    <i class="fas fa-bars"></i>
+                        Web apps
+                    <i id = 'down' class="fas fa-angle-down">
+                        {document.getElementById("apps").addEventListener("click")}
+                    </i>
+                    <ul id = 'dropmenu'>
+                        <li>React app</li>
+                        <li>Dojo app</li>
+                    </ul>
                 </div>
             </div>
             <div className = 'sadrzaj'>
@@ -24,5 +32,7 @@ const Toolbar = props => (
             </div>
         </main>
     </div>
+
+
 );
 export default Toolbar;
